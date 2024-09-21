ee6acd0b273b069f28f8caa1b6f98755e7a5adff
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-21 15:13:13 +0300

    updated: card view design

--- app/src/main/java/com/ismailmesutmujde/kotlinrecyclerview/MainActivity.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlinrecyclerview/MainActivity.kt
@@ -2,12 +2,18 @@ package com.ismailmesutmujde.kotlinrecyclerview
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.ismailmesutmujde.kotlinrecyclerview.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bindingMain : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bindingMain = ActivityMainBinding.inflate(layoutInflater)
+        val view = bindingMain.root
+        setContentView(view)
 
     }
 }
